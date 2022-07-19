54c4177f020e0009e5bba49e41c5f301c2e5e88f
David Taing <66336324+dat-monke@users.noreply.github.com>
2022-07-19 13:55:38 -0700

    completed 2.2

--- Exercise Files/Ch02/02_02/start/global.js
+++ Exercise Files/Ch02/02_02/start/global.js
@@ -1,3 +1,16 @@
-let hello = "Hello World from Node.js";
-let justNode = hello.slice(17);
-console.log(`Who let the ${justNode} out?`);
+const path = require("path");
+
+// This is the relative path up till the file
+console.log(__dirname);
+// This is the complete path for the document
+console.log(__filename);
+
+// This will have a pre-formatted string
+// Using path, you are able to grab only the last parts
+console.log(`The file name is ${path.basename(__filename)}`);
+// OUTPUT: The file name is global.js
+
+// This tells you what is running
+for (let key in global) {
+  console.log(key);
+}
