24e2349ade27bcc4775cee9f550a008b87af1415
David Taing <66336324+dat-monke@users.noreply.github.com>
2022-07-19 14:01:28 -0700

    value isolation | complete 2.3

--- Exercise Files/Ch02/02_03/start/global.js
+++ Exercise Files/Ch02/02_03/start/global.js
@@ -1,10 +1,17 @@
-const path = require("path");
+// This prints out the processes
+// console.log(process.argv);
 
-console.log(__dirname);
-console.log(
-  `The file name is ${path.basename(__filename)}`
-);
-
-for (let key in global) {
-  console.log(key);
+function grab(flag) {
+  // Gives us the position after the index
+  // 'Eve' | 'hello from node'
+  let indexAfterFlag = process.argv.indexOf(flag) + 1;
+  return process.argv[indexAfterFlag];
 }
+
+// Gets the value of greeting and user
+let greeting = grab("--greeting");
+let user = grab("--user");
+
+// Prints out the greeting and user once it has been grabbe
+console.log(greeting);
+console.log(user);
