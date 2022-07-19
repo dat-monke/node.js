1d36f2075236bf90a21d435368170a1116ea4fa7
David Taing <66336324+dat-monke@users.noreply.github.com>
2022-07-19 15:27:34 -0700

    completed 3.6 making your files modular so it can be used elsewhere

--- Exercise Files/Ch03/03_06/start/ask.js
+++ Exercise Files/Ch03/03_06/start/ask.js
@@ -1,28 +1,4 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function collectAnswers(questions, done) {
-  const answers = [];
-  const [firstQuestion] = questions;
-
-  const questionAnswered = (answer) => {
-    answers.push(answer.trim());
-    if (answers.length < questions.length) {
-      rl.question(
-        questions[answers.length],
-        questionAnswered
-      );
-    } else {
-      return done(answers);
-    }
-  };
-
-  rl.question(firstQuestion, questionAnswered);
-}
-
+const collectAnswers = require("./lib/collectAnswers");
 const questions = [
   "What is your name?",
   "Where do you live?",
@@ -34,3 +10,5 @@ collectAnswers(questions, (answers) => {
   console.log(answers);
   process.exit();
 });
+
+// Nothing really happened in here
