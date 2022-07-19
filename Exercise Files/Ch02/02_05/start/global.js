febc46fa6e635d90bec53a6345fe4b5648410124
David Taing <66336324+dat-monke@users.noreply.github.com>
2022-07-19 14:15:17 -0700

    string formatting can't test on the sandbox, but can do it on the local environment {probably}

--- Exercise Files/Ch02/02_05/start/global.js
+++ Exercise Files/Ch02/02_05/start/global.js
@@ -7,13 +7,37 @@ const questions = [
 ];
 const answers = [];
 
-function ask(i) {
+ask();
+
+// Basically setting the loop
+function ask(i = 0) {
   process.stdout.write(`\n\n\n ${questions[i]}`);
   process.stdout.write(` > `);
 }
 
+// Here you are collecting the answers until there is no more to ask
 process.stdin.on("data", function (data) {
-  process.stdout.write(data.toString().trim());
+  answers.push(data.toString().trim());
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
 });
 
-ask(answers.length);
+// Grabbing all the answers from previous and putting it into preformatted string
+process.on("exit", function () {
+  process.stdout.write("\n\n\n\n");
+  process.stdout.write(
+    `Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later`
+  );
+});
+
+/* ======================================== 
+SAMPLE OUTPUT: 
+What is your name? > Eve
+What would you rather be doing? > Skiing 
+What is your preffered programming language? > JavaScript 
+
+Go Skiiing Eve you can finish writing JavaScript later
+=========================================== */
