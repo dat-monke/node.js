158290f979ac9fc1c152aef91dedc99e159453c8
David Taing <66336324+dat-monke@users.noreply.github.com>
2022-07-19 13:50:03 -0700

    removing 'finished' | completed 2.1

--- Exercise Files/Ch02/02_01/start/global.js
+++ Exercise Files/Ch02/02_01/start/global.js
@@ -1,3 +1,6 @@
 let hello = "Hello World from Node.js";
+// This takes everything past the 17th item; Node.js
 let justNode = hello.slice(17);
+// When you do ``, it means you are pre-formatting the string
+// When you do ${} it basically allows you to put the variable in there
 console.log(`Who let the ${justNode} out?`);
