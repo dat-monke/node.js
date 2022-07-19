8560925a3fa564ce8343a389668ae74dfcf5f10f
David Taing <66336324+dat-monke@users.noreply.github.com>
2022-07-19 14:07:07 -0700

    completed 2.4 can't be done on the sandbox, but usually this will be shown via the console/terminal locally

--- Exercise Files/Ch02/02_04/start/global.js
+++ Exercise Files/Ch02/02_04/start/global.js
@@ -1,10 +1,25 @@
-function grab(flag) {
-  let indexAfterFlag = process.argv.indexOf(flag) + 1;
-  return process.argv[indexAfterFlag];
+// This types out in the output: Hello
+process.stdout.write("Hello \n \n");
+
+// These are the questions that are to be asked
+const questions = [
+  "what is your name",
+  "what are you doing",
+  "what is your pref language"
+];
+
+// This is an empty array where the responses are collected
+const answers = [];
+
+// A function with a preformatted string that ask the questions
+function ask(i) {
+  process.stdout.write(`\n\n\n ${questions[i]}`);
 }
 
-let greeting = grab("--greeting");
-let user = grab("--user");
+// Takes the response, trims whitespace, then puts it back out in the terminal
+process.stdin.on("data", function (data) {
+  process.stdout.write(data.toString().trim());
+});
 
-console.log(greeting);
-console.log(user);
+// Asks all the questions and answers
+ask(answers.length);
